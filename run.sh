c96109f95e7354155de941361a16bda2a3ab976f
icqparty <icqparty@gmail.com>
2020-01-24 09:32:08 +0300

    refactoring

--- run.sh
+++ run.sh
@@ -34,16 +34,16 @@ then
     while read p; do
 
 
-        repo_url_complete=$(echo $URL$p | cut -d'=' -f1 | xargs)
-        repo_token_complete=$(echo $p | cut -d'=' -f2 | xargs)
+        repo_url=$(echo $URL$p | cut -d'=' -f1 | xargs)
+        repo_token=$(echo $p | cut -d'=' -f2 | xargs)
 
         repo_name="$(echo $p | cut -d'=' -f1 | xargs)$index"
 
         echo "repo_name=$repo_name"
-        echo "repo_url_complete=$repo_url_complete"
-        echo "repo_token_complete=$repo_token_complete"
+        echo "repo_url=$repo"
+        echo "repo_token=$repo_token_complete"
         echo ""
-        echo "docker run -rm -d --restart=always -e REPO_URL="$repo_url_complete" -e RUNNER_TOKEN="$repo_token_complete" -v /var/run/docker.sock:/var/run/docker.sock --name $repo_name -t github-runner:latest"
+        echo "docker run -rm -d --restart=always -e REPO_URL="$repo_url" -e RUNNER_TOKEN="$repo_token" -v /var/run/docker.sock:/var/run/docker.sock --name $repo_name -t github-runner:latest"
         echo ""
 
 
@@ -51,7 +51,7 @@ then
         docker stop $repo_name && docker rm $repo_name
 
 
-        docker run -d --rm  -e REPO_URL="$repo_url_complete" -e RUNNER_TOKEN="$repo_token_complete" -v /var/run/docker.sock:/var/run/docker.sock --name $repo_name -t github-runner:latest
+        docker run -d --rm  -e REPO_URL="$repo_url" -e RUNNER_TOKEN="$repo_token" -v /var/run/docker.sock:/var/run/docker.sock --name $repo_name -t github-runner:latest
 
 
         #REPO_NAME=${repo_name}  REPO_URL=${repo_url_complete}  REPO_TOKEN=${repo_token_complete}  docker-compose up -d --force-recreate --remove-orphans
